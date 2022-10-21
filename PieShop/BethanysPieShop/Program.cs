c855901b700b12f3616dec36ae7ea53e86b0df8f
PLANETDDS\mmamo <mmamo@planetdds.com>
2022-10-21 15:44:36 -0700

    Finishing the settings

--- PieShop/BethanysPieShop/Program.cs
+++ PieShop/BethanysPieShop/Program.cs
@@ -1,6 +1,31 @@
 var builder = WebApplication.CreateBuilder(args);
+//makes sure that the application takes a look at the appsettings.json
+//loads the default log settings
+//set up iis integration
+//www for static files
+//access to collection services
+
+builder.Services.AddControllersWithViews(); 
+//makes sure that the application knows about MVC
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+//configured to look for static files
+// in the www root and short circuits requests
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+//diagnostic middleware component
+//might contain sensitive info
+
+app.MapDefaultControllerRoute();
+//end point middleware
+//set defaults used in mvc to the views
+//to make sure we see our views
 
 app.Run();
